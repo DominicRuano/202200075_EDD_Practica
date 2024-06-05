fac722640f5acc5f3e243251c102b12e8989b6f1
DominicR <Dojuparupe1975@gmail.com>
2024-06-05 00:47:31 -0600

    feat: Add JSON file reading functionality Se leer json, aunque de momento solo se implenento con un cout, queda a la espera de la implementacion de las estructuras.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "./Objects/nodo.h"
 #include <limits>
+#include <fstream>
+#include "./json.hpp"
 using namespace std;
+
+using json = nlohmann::json;
 
 /* Funcion que despliega el menu principal. */
 int Menu();
+/* Funcion que lee un json. */
+void ReadJson();
 
 int main(){
     int input; // Guarda el valor seleccionado por el usuario.
@@ -40,6 +46,7 @@ int main(){
             break;
         case 6:
             cout << "Se selecciono 6." << endl;
+            ReadJson();
             // Funcion aqui.
             break;
         default:
@@ -48,6 +55,35 @@ int main(){
         }
     }
     return 0;
+}
+
+void ReadJson(){
+    string filePath = "Aviones.json"; // Ruta del archivo JSON
+
+    // Leer el archivo JSON
+    ifstream inputFile(filePath);
+    if (!inputFile.is_open()) {
+        std::cerr << "Could not open the file!" << std::endl;
+    }
+
+    // Parsear el JSON
+    json jsonData;
+    inputFile >> jsonData;
+
+    // Acceder a los datos
+    for (const auto& item : jsonData) { // Cambiar todo esto por una append a la estructura.
+        std::cout << "Vuelo: " << item["vuelo"] << std::endl;
+        std::cout << "Numero de Registro: " << item["numero_de_registro"] << std::endl;
+        std::cout << "Modelo: " << item["modelo"] << std::endl;
+        std::cout << "Fabricante: " << item["fabricante"] << std::endl;
+        std::cout << "Ano de Fabricacion: " << item["ano_fabricacion"] << std::endl;
+        std::cout << "Capacidad: " << item["capacidad"] << std::endl;
+        std::cout << "Peso Max Despegue: " << item["peso_max_despegue"] << std::endl;
+        std::cout << "Aerolinea: " << item["aerolinea"] << std::endl;
+        std::cout << "Estado: " << item["estado"] << std::endl;
+        std::cout << "---------------------------" << std::endl;
+    }
+
 }
 
 int Menu(){
