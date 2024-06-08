90e539e28ffdd7fd4bae200ba80522404505030d
DominicR <Dojuparupe1975@gmail.com>
2024-06-07 18:47:28 -0600

    QOL commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>  // Para getch()
 #include "./Structs/CircularDoublyLinkedList.h"
 #include "./Objects/avion.h"
 #include <limits>
@@ -8,68 +9,92 @@ using namespace std;
 
 using json = nlohmann::json;
 
-/* Funcion que despliega el menu principal. */
-int Menu();
-/* Funcion que lee un json. */
-void ReadJson(CircularDoublyLinkedList<avion> &listaAviones,CircularDoublyLinkedList<avion> &listaAviones2);
+
+
+/*Funcion que recibe un puntero de tipo generico.*/
+template <typename T>
+void GetOp(T &input);
+
+/* Funcion que despliega el menu principal.
+    Recibe como parametro una direccion en la cual se guardara la opcion seleccionada.*/
+void Menu(int &input);
+
+/* Funcion que lee un json. 
+    Recibe como parametro un string con el path del json y dos lista, aviones disponibles, aviones en mantenimiento*/
+void ReadJson(string filePath, CircularDoublyLinkedList<avion> &listaAviones, CircularDoublyLinkedList<avion> &listaAviones2);
+
+/*Funcion para la opcion cargar Aviones.*/
+bool CargaAviones(CircularDoublyLinkedList<avion> &listaAviones, CircularDoublyLinkedList<avion> &listaAviones2);
 
 int main(){
-    int input; // Guarda el valor seleccionado por el usuario.
-    int stop;
-    CircularDoublyLinkedList<avion> listaAvionesDisponibles;
-    CircularDoublyLinkedList<avion> listaAvionesMantenimiento;
+    int input;      // Guarda el valor seleccionado por el usuario.
+    bool cond;   // Condicion para salir del ciclo.
+
+    CircularDoublyLinkedList<avion> listaAvionesDisponibles;    /*Guarda aviones disponibles.*/
+    CircularDoublyLinkedList<avion> listaAvionesMantenimiento;  /*Guarda aviones en mantenimiento.*/
+
     while (input != 6){
-        Menu();
-        cin >> input;
-        // Maneja errores.
-        if (cin.fail() || cin.peek() != '\n' || input < '0' || input > '9') {
-            cin.clear(); // Clear the error flag
-            cin.ignore(std::numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-        }
+        cond = false;
+
+        Menu(input);
         switch (input){
         case 1:
-            cout << "Se selecciono 1." << endl;
-            // Funcion aqui.
+            do
+                cond = CargaAviones(listaAvionesDisponibles,listaAvionesMantenimiento);
+            while (cond == false);
             break;
         case 2:
-            cout << "Se selecciono 2" << endl;
             // Funcion aqui.
             break;
         case 3:
-            cout << "Se selecciono 3" << endl;
             // Funcion aqui.
             break;
         case 4:
-            cout << "Se selecciono 4" << endl;
             // Funcion aqui.
             break;
         case 5:
-            cout << "Se selecciono 5" << endl;
-            // Funcion aqui.
-            break;
-        case 6:
-            cout << "Se selecciono 6." << endl;
-            ReadJson(listaAvionesDisponibles,listaAvionesMantenimiento);
             cout << "\n - Lista de Aviones En mantenimiento: " << endl;
             listaAvionesMantenimiento.print();
             cout << "\n - Lista de Aviones Disponibles: " << endl;
             listaAvionesDisponibles.print();
+
+            cout << "Presiona Enter para continuar...";
+            _getch();  // Espera a que el usuario presione cualquier tecla
+            break;
+        case 6:
             break;
         default:
-            // Funcion aqui.
             break;
         }
     }
     return 0;
 }
 
-void ReadJson(CircularDoublyLinkedList<avion> &listaAviones,CircularDoublyLinkedList<avion> &listaAviones2){
-    string filePath = "./ArchivosEntrada/Aviones.json"; // Ruta del archivo JSON
+bool CargaAviones(CircularDoublyLinkedList<avion> &listaAviones, CircularDoublyLinkedList<avion> &listaAviones2){
+    string path;
+    cout << "\tIngrese la ruta del archivo JSON: ";
+    GetOp(path);
+    try{
+        ReadJson(path,listaAviones,listaAviones2);
+    }catch(const std::exception& e){
+        return false;
+    }
+    cout << "\n\tSe tienen (" << listaAviones.getLength() << ") aviones en la lista de aviones disponibles." << endl;
+    cout << "\tSe tienen (" << listaAviones2.getLength() << ") aviones en la lista de aviones en mantenimiento." << endl;
+
+    cout << "Presiona Enter para continuar...";
+    _getch();  // Espera a que el usuario presione cualquier tecla
+    return true;
+
+}
+
+void ReadJson(string filePath, CircularDoublyLinkedList<avion> &listaAviones, CircularDoublyLinkedList<avion> &listaAviones2){
+    //string filePath = "./ArchivosEntrada/Aviones.json"; // Ruta del archivo JSON
 
     // Leer el archivo JSON
     ifstream inputFile(filePath);
     if (!inputFile.is_open()) {
-        std::cerr << "Could not open the file!" << std::endl;
+        cout << "\tError al cargar el archivo JSON." << endl;
     }
 
     // Parsear el JSON
@@ -86,7 +111,7 @@ void ReadJson(CircularDoublyLinkedList<avion> &listaAviones,CircularDoublyLinked
     }
 }
 
-int Menu(){
+void Menu(int &input){
     system("cls");
     cout << "------------ MENU --------------" << endl;
     cout << "1. Carga de Aviones." << endl;
@@ -95,4 +120,17 @@ int Menu(){
     cout << "4. Consulta Pasajeros." << endl;
     cout << "5. Visualizar Reportes." << endl;
     cout << "6. Salir." << endl;
+
+    cout << "Seleccione una opcion: ";
+    GetOp(input);
+}
+
+template <typename T>
+void GetOp(T &input){
+    cin >> input;
+    // Maneja errores.
+    if (cin.fail() || cin.peek() != '\n') {
+        cin.clear(); // Clear the error flag
+        cin.ignore(std::numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+    }
 }
